936af408c08009a4ad483d1c350463f1bc03c42a
Baohe Zhang <zhangb@tfpool28.informatik.uni-freiburg.de>
2018-11-28 01:07:16 +0100

    hl=1

--- exercise3_R_NR/train_agent.py
+++ exercise3_R_NR/train_agent.py
@@ -203,7 +203,7 @@ if __name__ == "__main__":
 
     # read data    
     X_train, y_train, X_valid, y_valid = read_data("./data")
-    history_length = 3
+    history_length = 1
     # preprocess data
     X_train, y_train, X_valid, y_valid = preprocessing(X_train, y_train, X_valid, y_valid, history_length)
     # train model (you can change the parameters!)
